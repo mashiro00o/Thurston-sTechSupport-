d81c2a90c21e9da9867a33ae46a91371f6f7d259
mashiro00o <125548786+mashiro00o@users.noreply.github.com>
2024-02-20 19:55:41 +0800

    Update List&Array.py

--- Basics/List&Array.py
+++ Basics/List&Array.py
@@ -5,22 +5,22 @@ my_array = []
 my_array.append(1)
 my_array.append(2)
 my_array.append(3)
-print(my_array) # Array after adding elements: [1, 2, 3]
+print(my_array) # Output array after adding elements
 
 # Read - Accessing elements of the array
-print(my_array[1]) # Element at index 1: 2
+print(my_array[1]) # Output element at index 1
 
 # Update - Modifying elements of the array
 my_array[1] = 10
-print(my_array) # Array after updating element at index 1: [1, 10, 3]
+print(my_array) # Output array after updating element at index 1
 
 # Delete - Removing elements from the array
 my_array.pop()  # Remove the last element
-print(my_array) # Array after deleting the last element: [1, 10]
+print(my_array) # Output array after deleting the last element
 
 # Remove the element at index 0
 del my_array[0]
-print(my_array) # Array after deleting the first element: [10]
+print(my_array) # Output array after deleting the first element
 
 # output
 # [1, 2, 3]
